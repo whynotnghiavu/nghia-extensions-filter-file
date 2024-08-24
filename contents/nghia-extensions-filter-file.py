d92cd77f992d9906a1e3ac9d207d738dd80de76d
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-24 11:51:53 +0700

    create 1 file and update 1 file

--- contents/nghia-extensions-filter-file.py
+++ contents/nghia-extensions-filter-file.py
@@ -9,7 +9,7 @@ import shutil
 
 
 # Đường dẫn đến folder
-folder_path = r"C:\Users\vvn20206205\Desktop\rrrr\FormatFileSub"
+folder_path = r"C:\Users\vvn20206205\Desktop\rrrr\HopNhatCau"
 
 
 # Tạo thư mục "_nghia_ok"
