bd4dd73331115302bae1225627e369dd9c47fa7e
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-24 17:03:27 +0700

    delete 1 file and update 1 file

--- contents/nghia-extensions-filter-file.py
+++ contents/nghia-extensions-filter-file.py
@@ -9,7 +9,7 @@ import shutil
 
 
 # Đường dẫn đến folder
-folder_path = r"C:\Users\vvn20206205\Desktop\rrrr\MyTextToSpeech"
+folder_path = r"C:\Users\vvn20206205\Desktop\rrrr\TinhGiay"
 
 
 # Tạo thư mục "_nghia_ok"
